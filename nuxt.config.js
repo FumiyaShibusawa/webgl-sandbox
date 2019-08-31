7d5ad7b1276ed3531a5fabb283047686ec4fa893
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2019-09-01 00:35:27 +0900

    feat: set default reset.css, meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,17 @@ export default {
   server: {
     port: 4000,
     host: '0.0.0.0'
+  },
+  css: [
+    '@/assets/css/reset.css'
+  ],
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' }
+    ]
   }
 }
